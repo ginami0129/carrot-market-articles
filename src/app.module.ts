e26c6b0808e7cbc7b995b232b6ec3e869252d565
권기남 <ginam@gwonginam-ui-MacBookPro.local>
2022-06-26 11:31:02 +0900

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.number().required(),
+        MYSQL_USER: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+    ArticlesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
