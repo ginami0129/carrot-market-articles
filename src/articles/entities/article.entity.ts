bf9a7869d6abbe393b1781109b426cd1ac4dbb9e
권기남 <ginam@gwonginam-ui-MacBookPro.local>
2022-06-26 11:38:11 +0900

    Add article entity

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,25 @@
-export class Article {}
+// 게시글 고유 번호
+// 제목
+// 본문
+// 생성 시각
+// 수정 시각
+
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// 삭제 시각
+export class Article {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public title: string;
+
+  @Column()
+  public desc: string;
+
+  @Column()
+  public createdAt: Date;
+
+  @Column()
+  public updatedAt: Date;
+}
